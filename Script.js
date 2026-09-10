78982f5ee498132df49ddb7c35b18948a9a5ced7
Vaundra <vasundrarasikaran@gmail.com>
2026-09-10 14:00:40 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,10 @@
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", function () {
+
+    const city = cityInput.value;
+
+    console.log(city);
+
+});
