3b3d4dddc12de3e0f53c72918f4f1d4f456e6cb9
Vaundra <vasundrarasikaran@gmail.com>
2026-09-11 14:47:01 +0530

    Update

--- Script.js
+++ Script.js
@@ -7,4 +7,17 @@ searchBtn.addEventListener("click", function () {
 
     console.log(city);
 
+    const url = ;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+
 });
